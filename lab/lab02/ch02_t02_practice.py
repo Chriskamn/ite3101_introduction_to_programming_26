4a6256dc79101bd53b66f165e25df4b87843dc87
Chriskamn <76195047+Chriskamn@users.noreply.github.com>
2026-09-17 08:23:56 +0000

    [skip actions] lab/lab02/ch02_t03_escaping_characters.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -4,6 +4,7 @@ praline = "John"
 viking = "Teresa"
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
-# print(caesar)
-# print(praline)
-# print(viking)
+print(caesar)
+print(praline)
+print(viking)
+
